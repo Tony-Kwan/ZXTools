676e1e632799598d785bad86414aa882b177fe79
Tony-Kwan <15813669070@163.com>
2018-03-08 15:21:29 +0800

    GeoPoint override equals & hashCode

--- src/main/java/com/pygzx/zxtools/util/geo/GeoPoint.java
+++ src/main/java/com/pygzx/zxtools/util/geo/GeoPoint.java
@@ -30,4 +30,30 @@ public class GeoPoint {
 	boolean isLatLngEqual(GeoPoint point) {
 		return Math.abs(longitude - point.longitude) < INFS && Math.abs(latitude - point.latitude) < INFS;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		GeoPoint point = (GeoPoint) o;
+
+		if (Double.compare(point.longitude, longitude) != 0) {
+			return false;
+		}
+		return Double.compare(point.latitude, latitude) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		temp = Double.doubleToLongBits(longitude);
+		result = (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(latitude);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 }
